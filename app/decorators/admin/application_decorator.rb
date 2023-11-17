5ea6307a4b38fabded8bf646c6879bb0c9d8ac1f
Ryan Bates <ryan@railscasts.com>
2023-11-17 13:27:26 -0800

    Generate wallaby admin files

---
+++ app/decorators/admin/application_decorator.rb
@@ -0,0 +1,10 @@
+module Admin
+  # @see https://github.com/wallaby-rails/wallaby/blob/master/docs/decorator.md
+  class ApplicationDecorator < Wallaby::ResourceDecorator
+    # # base/abstract class?
+    # base_class!
+
+    # # set namespace
+    # self.namespace = 'Admin'
+  end
+end
